c54ebdcc5319236f77049e4bec1e7fada0a335d3
Егор <egor.zholobov@yandex.ru>
2022-11-17 13:14:35 +0300

    Добавил базу данных

--- WpfApp/Views/Pages/NavigationPage.xaml.cs
+++ WpfApp/Views/Pages/NavigationPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Infrastructure.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace WpfApp.Views.Pages
 {
     /// <summary>
@@ -23,6 +25,7 @@ namespace WpfApp.Views.Pages
         public NavigationPage()
         {
             InitializeComponent();
+           
         }
     }
 }
