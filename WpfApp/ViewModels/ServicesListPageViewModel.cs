5ab86f907dd95a5adbef473f4669af2268ccb16d
Егор <egor.zholobov@yandex.ru>
2022-12-15 22:30:38 +0300

    Мелкие фиксы

--- WpfApp/ViewModels/ServicesListPageViewModel.cs
+++ WpfApp/ViewModels/ServicesListPageViewModel.cs
@@ -21,9 +21,12 @@ namespace WpfApp.ViewModels
     public class ServicesListPageViewModel:BaseViewModel
     {
         List<ServicesListItemModel> list = new List<ServicesListItemModel>();
+        private List<ServicesListItemModel> searchlist = new List<ServicesListItemModel>();
+       
         public   ServicesListPageViewModel()
         {
-
+            Selectedsort = sorts[0];
+            Selectedfilter = filters[0];
             LoadData();
         }
 
@@ -182,6 +185,118 @@ namespace WpfApp.ViewModels
 
                 });
             }
+        }
+        private string search;
+
+        public string Search
+        {
+            get { return search; }
+            set
+            {
+                search = value;
+                OnPropertyChanged();
+                if (!String.IsNullOrEmpty(value))
+                {
+                    searchlist = list.Where(x => x.Title.ToLower().Contains(value.ToLower())).ToList();
+                    Services = searchlist;
+                }
+                else
+                {
+                    Services = searchlist;
+                }
+            }
+        }
+
+
+        private List<FilterModel> filters = new List<FilterModel>()
+        {
+            new FilterModel { Id = 0, Name = "Фильтр" },
+            new FilterModel { Id = 1, Name = "от 0 до 5%"},
+            new FilterModel { Id = 2, Name = "от 5% до 15%"},
+            new FilterModel { Id = 3, Name = "от 15% до 30%"},
+            new FilterModel { Id = 4, Name = "от 30% до 70%"},
+            new FilterModel { Id = 5, Name = "от 70% до 100%"}
+        };
+        public List<FilterModel> Filters
+        {
+            get { return filters; }
+            set
+            {
+                filters = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private FilterModel selectedfilter;
+        public FilterModel Selectedfilter
+        {
+            get { return selectedfilter; }
+            set
+            {
+                selectedfilter = value;
+                OnPropertyChanged();
+                if (selectedfilter.Id == 1)
+                {
+                    searchlist = list.Where(x => x.Discount >= 0 && x.Discount <= 5).ToList();
+                }
+                else if (selectedfilter.Id == 2)
+                {
+                    searchlist = list.Where(x => x.Discount > 5 && x.Discount <= 15).ToList();
+                }
+                else if (selectedfilter.Id == 3)
+                {
+                    searchlist = list.Where(x => x.Discount > 15 && x.Discount <= 30).ToList();
+                }
+                else if (selectedfilter.Id == 4)
+                {
+                    searchlist = list.Where(x => x.Discount > 30 && x.Discount <= 70).ToList();
+                }
+                else if (selectedfilter.Id == 5)
+                {
+                    searchlist = list.Where(x => x.Discount > 70 && x.Discount <= 100).ToList();
+                }
+                else
+                {
+                    searchlist = list;
+                }
+
+                Services = searchlist;
+            }
+        }
+
+        private List<SortModel> sorts = new List<SortModel>() {
+            new SortModel { Id = 0, Name = "Сортировка" },
+            new SortModel { Id = 1, Name = "По возрастанию" },
+            new SortModel { Id = 2, Name = "По убыванию" },};
+        public List<SortModel> Sorts
+        {
+            get { return sorts; }
+            set
+            {
+                sorts = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private SortModel selectedsort;
+        public SortModel Selectedsort
+        {
+            get { return selectedsort; }
+            set
+            {
+                selectedsort = value; OnPropertyChanged();
+                if (selectedsort.Id == 1)
+                {
+                    searchlist = list.OrderBy(x => x.Cost).ToList();
+                    Services = searchlist;
+                }
+                else if (selectedsort.Id == 2)
+                {
+                    searchlist = list.OrderBy(x => x.Cost).Reverse().ToList();
+                    Services = searchlist;
+                }
+            }
+
         }
 
     }
